8b6b8e3548268ca51237ce55ad35290929ac7fb4
AnnaSergeenkova <lutekAnn@yandex.ru>
2021-10-19 22:38:05 +0300

    hw

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,9 +1,12 @@
 package ru.skypro;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	        // Задача 1
+        // Задача 1
         {
             int i = 0;
             while (i < 10) {
@@ -18,37 +21,38 @@ public class Main {
 
         // Задача 2
         System.out.println();
-        for (int a = 1; a <= 31;  a = a + 7) {
+        for (int a = 1; a <= 31; a += 7) {
             System.out.println("Сегодня пятница " + a + "-е. Время готовить еженедельный отчет!");
         }
 
         //Задача 3
-        System.out.println();
-        int n = 2021;
-        int y = n-200;
-        int def = n-1975;
-        for (int x = n - def; x > y && x <= n; x = x - 79) {
-            System.out.println(x);
-        }
-        for (int x = n - def + 79; x > y && x <n+100; x = x + 79) {
-            System.out.println(x);
-        }
+        {
+            System.out.println();
+            int currentYear = 2021;
+            int start = currentYear - 200;
+            int ending = currentYear + 100;
+            for (int i = start; i < ending; i++) {
+                if (i % 79 == 0) {
+                    System.out.println(i);
+                }
+            }
 
-        // Задание 4
-        System.out.println();
-        int i = 1;
-        for (i = 1; i <= 30; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println(i + ": ping pong");
-            } else if (i % 3 == 0) {
-                System.out.println(i + ": ping");
-            } else if (i % 5 == 0) {
-                System.out.println(i + ": pong");
+            // Задание 4
+            System.out.println();
+            int i = 1;
+            for (i = 1; i <= 30; i++) {
+                System.out.print(i + ":");
+                if (i % 3 ==0) {
+                    System.out.print("ping" + " ");
+                }
+                if (i % 5 == 0) {
+                    System.out.print("pong" + " ");
+                }
+                System.out.println();
+            }
 
-            } else
-                System.out.println(i + ": ");
+
         }
-
     }
 }
 
