7c25987d7d35f99d9847768eb5326f3db0db9443
AnnaSergeenkova <lutekAnn@yandex.ru>
2021-11-05 21:30:44 +0300

    Добавление equals и hashCode, метод toString уже был изначально

--- src/ru/skypro/lesson9/Author.java
+++ src/ru/skypro/lesson9/Author.java
@@ -1,7 +1,23 @@
 package ru.skypro.lesson9;
 
+import java.util.Objects;
+
 public class Author {
     private final String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
+
     private final String surname;
 
     public Author(String name, String surname) {
--- src/ru/skypro/lesson9/Book.java
+++ src/ru/skypro/lesson9/Book.java
@@ -1,8 +1,24 @@
 package ru.skypro.lesson9;
 
+import java.util.Objects;
+
 public class Book {
 
     private int publishingYear;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishingYear == book.publishingYear && title.equals(book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(publishingYear, title);
+    }
+
     private final String title;
 
 
